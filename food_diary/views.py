9fca157d215855d6b4e8cf35391439c8b16b5fe7
David Morgan <david.r.morgan.92@gmail.com>
2024-02-28 17:06:34 +0000

    Refactor food diary index view

--- food_diary/views.py
+++ food_diary/views.py
@@ -5,8 +5,6 @@ from .models import FoodDiary, FoodDiaryEntry
 
 @login_required
 def index(request):
-    food_diary_query = FoodDiary.objects.filter(owner__exact=request.user.id)
-
     context = {
         'breakfast': [],
         'lunch': [],
@@ -14,21 +12,20 @@ def index(request):
         'snacks': []
     }
 
-    if request.GET:
-        if 'date' in request.GET:
-            food_diary_query = food_diary_query.filter(
-                date__exact=request.GET['date'])
-            food_diary = food_diary_query.first()
+    if request.GET and 'date' in request.GET:
+        food_diary = FoodDiary.objects.filter(
+            owner__exact=request.user.id,
+            date__exact=request.GET['date']).first()
 
-            if food_diary != None:
-                context['breakfast'] = list(food_diary.fooddiaryentry_set.filter(
-                    meal__exact=FoodDiaryEntry.Meal.BREAKFAST))
-                context['lunch'] = list(food_diary.fooddiaryentry_set.filter(
-                    meal__exact=FoodDiaryEntry.Meal.LUNCH))
-                context['dinner'] = list(food_diary.fooddiaryentry_set.filter(
-                    meal__exact=FoodDiaryEntry.Meal.DINNER))
-                context['snacks'] = list(food_diary.fooddiaryentry_set.filter(
-                    meal__exact=FoodDiaryEntry.Meal.SNACKS))
+        if food_diary != None:
+            context['breakfast'] = list(food_diary.fooddiaryentry_set.filter(
+                meal__exact=FoodDiaryEntry.Meal.BREAKFAST))
+            context['lunch'] = list(food_diary.fooddiaryentry_set.filter(
+                meal__exact=FoodDiaryEntry.Meal.LUNCH))
+            context['dinner'] = list(food_diary.fooddiaryentry_set.filter(
+                meal__exact=FoodDiaryEntry.Meal.DINNER))
+            context['snacks'] = list(food_diary.fooddiaryentry_set.filter(
+                meal__exact=FoodDiaryEntry.Meal.SNACKS))
 
     return render(request, 'food_diary/index.html', context)
 
