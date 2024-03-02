b7470d97048d6f205b051135c205bcb23e29cb03
David Morgan <david.r.morgan.92@gmail.com>
2024-03-02 14:44:21 +0000

    Load date into food diary control

--- food_diary/views.py
+++ food_diary/views.py
@@ -1,31 +1,39 @@
 from django.shortcuts import render
 from allauth.account.decorators import login_required
+import datetime
 from .models import FoodDiary, FoodDiaryEntry
 
 
 @login_required
 def index(request):
+    # Load diary for the given date or today if none is given
+    date = request.GET['date'] if request.GET and 'date' in request.GET else datetime.date.today(
+    ).strftime('%Y-%m-%d')
+
+    # By default use empty arrays for the day's diary entries
     context = {
+        'date': date,
         'breakfast': [],
         'lunch': [],
         'dinner': [],
         'snacks': []
     }
 
-    if request.GET and 'date' in request.GET:
-        food_diary = FoodDiary.objects.filter(
-            owner__exact=request.user.id,
-            date__exact=request.GET['date']).first()
+    # Get the food diary for the requesting user with the chosen date
+    food_diary = FoodDiary.objects.filter(
+        owner__exact=request.user.id,
+        date__exact=date).first()
 
-        if food_diary != None:
-            context['breakfast'] = list(food_diary.fooddiaryentry_set.filter(
-                meal__exact=FoodDiaryEntry.Meal.BREAKFAST))
-            context['lunch'] = list(food_diary.fooddiaryentry_set.filter(
-                meal__exact=FoodDiaryEntry.Meal.LUNCH))
-            context['dinner'] = list(food_diary.fooddiaryentry_set.filter(
-                meal__exact=FoodDiaryEntry.Meal.DINNER))
-            context['snacks'] = list(food_diary.fooddiaryentry_set.filter(
-                meal__exact=FoodDiaryEntry.Meal.SNACKS))
+    # Populate the arrays by filtering the diary entries by their meal time
+    if food_diary != None:
+        context['breakfast'] = list(food_diary.fooddiaryentry_set.filter(
+            meal__exact=FoodDiaryEntry.Meal.BREAKFAST))
+        context['lunch'] = list(food_diary.fooddiaryentry_set.filter(
+            meal__exact=FoodDiaryEntry.Meal.LUNCH))
+        context['dinner'] = list(food_diary.fooddiaryentry_set.filter(
+            meal__exact=FoodDiaryEntry.Meal.DINNER))
+        context['snacks'] = list(food_diary.fooddiaryentry_set.filter(
+            meal__exact=FoodDiaryEntry.Meal.SNACKS))
 
     return render(request, 'food_diary/index.html', context)
 
