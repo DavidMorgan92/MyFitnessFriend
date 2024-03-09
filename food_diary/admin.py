2d2180d0827d438dab487840173fc4e8b0c51880
David Morgan <david.r.morgan.92@gmail.com>
2024-03-09 21:25:25 +0000

    Food diary admin

--- food_diary/admin.py
+++ food_diary/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
 from .models import FoodDiary, FoodDiaryEntry
 
-admin.site.register(FoodDiary)
-admin.site.register(FoodDiaryEntry)
+
+class FoodDiaryAdmin(admin.ModelAdmin):
+    list_display = (
+        'date',
+        'owner',
+    )
+
+    ordering = ('-date', 'owner')
+
+
+class FoodDiaryEntryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'food_diary',
+        'meal',
+        'calories',
+        'carbs_grams',
+        'fat_grams',
+        'protein_grams',
+        'sodium_milligrams',
+        'sugar_grams',
+    )
+
+    ordering = ('food_diary', 'name')
+
+
+admin.site.register(FoodDiary, FoodDiaryAdmin)
+admin.site.register(FoodDiaryEntry, FoodDiaryEntryAdmin)
