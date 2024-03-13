7ff28f1c1b549e6a06dab1e11f64986e19964dfb
David Morgan <david.r.morgan.92@gmail.com>
2024-03-13 22:14:31 +0000

    Use patch to mock environment variables

--- my_fitness_friend/tests.py
+++ my_fitness_friend/tests.py
@@ -1,29 +1,29 @@
 from django.test import TestCase
+from unittest.mock import patch
 from .database_router import DatabaseRouter
-from os import environ
 
 
 class DatabaseRouterTests(TestCase):
+    @patch.dict('os.environ', {'ENVIRONMENT': 'Production'})
     def test_db_for_read_returns_primary_given_production_environment(self):
-        environ['ENVIRONMENT'] = 'Production'
         router = DatabaseRouter()
         result = router.db_for_read(None)
         self.assertEqual('primary', result)
 
+    @patch.dict('os.environ', {'ENVIRONMENT': 'Development'})
     def test_db_for_read_returns_default_given_non_production_environment(self):
-        environ['ENVIRONMENT'] = 'Development'
         router = DatabaseRouter()
         result = router.db_for_read(None)
         self.assertEqual('default', result)
 
+    @patch.dict('os.environ', {'ENVIRONMENT': 'Production'})
     def test_db_for_write_returns_primary_given_production_environment(self):
-        environ['ENVIRONMENT'] = 'Production'
         router = DatabaseRouter()
         result = router.db_for_write(None)
         self.assertEqual('primary', result)
 
+    @patch.dict('os.environ', {'ENVIRONMENT': 'Development'})
     def test_db_for_write_returns_default_given_non_production_environment(self):
-        environ['ENVIRONMENT'] = 'Development'
         router = DatabaseRouter()
         result = router.db_for_write(None)
         self.assertEqual('default', result)
