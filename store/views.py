225795d83c0b1d6f13354475546bca5e2644e9f2
David Morgan <david.r.morgan.92@gmail.com>
2024-03-24 12:16:29 +0000

    Initialize search input with search term

--- store/views.py
+++ store/views.py
@@ -5,11 +5,12 @@ from .models import Product
 def index(request):
     if request.method == 'GET':
         if 'q' in request.GET:
-            products = Product.objects.filter(name_icontains=request.GET['q'])
+            products = Product.objects.filter(name__icontains=request.GET['q'])
         else:
             products = Product.objects.all()
 
     context = {
+        'search_term': request.GET['q'] if request.GET and 'q' in request.GET else '',
         'products': list(products),
     }
 
